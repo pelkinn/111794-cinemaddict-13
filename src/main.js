b922c663653104f55870d3c7c7420b920a19fd49
mrPelkin <mrpelkin@gmail.com>
2020-11-28 16:01:52 +0300

    Create mock data

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
 import {createRankUserTemplate} from './view/rank-user.js';
-import {createNavMenuTemplate} from './view/nav-menu.js';
+import {createGroupMenuTemplate} from './view/group-menu.js';
 import {createFilterTemplate} from './view/filter.js';
 import {createBoardTemplate} from './view/board.js';
 import {createBoardMainTemplate} from './view/board-main.js';
 import {createFilmTemplate} from './view/film.js';
 import {createButtonLoadMoreTemplate} from './view/button-load-more.js';
-import {createPopupFilmDetailsTemplate} from './view/popup-film-details';
+import {createPopupFilmDetailsTemplate} from './view/popup-film-details.js';
+import {generateFilm} from './mock/film.js';
 
-const FILMS_COUNT = 5;
+const FILMS_COUNT = 20;
+const FILMS_ITERATOR = 5;
+let index = 5;
+
+const films = new Array(FILMS_COUNT).fill().map(generateFilm);
+const filmsWatchingCount = films.filter(({isWatch}) => isWatch).length;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
 
-render(siteHeaderElement, createRankUserTemplate(), `beforeend`);
-render(siteMainElement, createNavMenuTemplate(), `beforeend`);
+if (filmsWatchingCount) {
+  render(siteHeaderElement, createRankUserTemplate(filmsWatchingCount), `beforeend`);
+}
+render(siteMainElement, createGroupMenuTemplate(films), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
@@ -28,9 +37,23 @@ render(filmsBoard, createBoardMainTemplate(), `beforeend`);
 const filmsBoardMain = filmsBoard.querySelector(`.films-list`);
 const filmsBoardMainContainer = filmsBoardMain.querySelector(`.films-list__container`);
 
-for (let i = 0; i < FILMS_COUNT; i++) {
-  render(filmsBoardMainContainer, createFilmTemplate(), `beforeend`);
+for (let i = 0; i < films.slice(0, FILMS_ITERATOR).length; i++) {
+  render(filmsBoardMainContainer, createFilmTemplate(films[i]), `beforeend`);
 }
 render(filmsBoardMain, createButtonLoadMoreTemplate(), `beforeend`);
-render(siteFooterElement, createPopupFilmDetailsTemplate(), `afterend`);
+// render(siteFooterElement, createPopupFilmDetailsTemplate(films[0]), `afterend`);
+
+const buttonLoadMoreFilms = document.querySelector(`.films-list__show-more`);
+buttonLoadMoreFilms.addEventListener(`click`, () => {
+  if (index < FILMS_COUNT) {
+    let nextFilms = films.slice(index, index + FILMS_ITERATOR);
+    for (let i = 0; i < nextFilms.length; i++) {
+      render(filmsBoardMainContainer, createFilmTemplate(nextFilms[i]), `beforeend`);
+    }
+    index += FILMS_ITERATOR;
+    if (index >= FILMS_COUNT) {
+      buttonLoadMoreFilms.style.display = `none`;
+    }
+  }
+});
 
---
+++ src/mock/const.js
@@ -0,0 +1,128 @@
+export const FILM_TITLES = [
+  `Made for each other`,
+  `Popeye meets sinbad`,
+  `Sagebrush trail`,
+  `Santa - Claus conquers the martians`,
+  `The dace of life`,
+  `The great flamarion`,
+  `The man with the golden arm`
+];
+
+export const FILM_SNIPPETS = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
+export const FILM_POSTERS = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+
+export const FILM_GENRES = [
+  `Musical`,
+  `Thriller`,
+  `Comedy`,
+  `Drama`,
+  `Horrors`,
+  `Fantasy`
+];
+
+export const FILM_DIRECTORS = [
+  `Стивен Спилберг`,
+  `Питер Джексон`,
+  `Мартин Скорсезе`,
+  `Кристофер Нолан`,
+  `Стивен Содерберг`,
+];
+
+export const FILM_SCREENWRITERS = [
+  `Билли Уайлдер`,
+  `Итан и Джоэл Коэны`,
+  `Роберт Таун`,
+  `Квентин Тарантино`,
+  `Фрэнсис Форд Коппола`,
+  `Уильям Голдман`,
+  `Чарли Кауфман`,
+  `Вуди Аллен`,
+  `Нора Эфрон`
+];
+
+export const FILM_ACTORS = [
+  `Хамфри Богарт`,
+  `Кэтрин Хепбёрн`,
+  `Бетт Дейвис`,
+  `Кэри Грант`,
+  `Джеймс Стюарт`,
+  `Одри Хепбёрн`,
+  `Марлон Брандо`,
+  `Ингрид Бергман`,
+  `Чарльз Чаплин`
+];
+
+export const FILM_COUNTRIES = [
+  `Австралия`,
+  `Австрия`,
+  `Бельгия`,
+  `Гаити`,
+  `Индия`,
+  `Испания`,
+  `Мавритания`,
+  `Румыния`,
+  `Сирия`
+];
+
+export const FILM_DESCRIPTIONS = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  `Повседневная практика показывает, что консультация с широким активом обеспечивает широкому кругу (специалистов) участие в формировании соответствующий условий активизации. Повседневная практика показывает, что новая модель организационной деятельности требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Товарищи! постоянный количественный рост и сфера нашей активности влечет за собой процесс внедрения и модернизации позиций, занимаемых участниками в отношении поставленных задач. Таким образом укрепление и развитие структуры позволяет оценить значение системы обучения кадров, соответствует насущным потребностям. Равным образом начало повседневной работы по формированию позиции требуют определения и уточнения направлений прогрессивного развития.`
+];
+
+export const FILM_COMMENTS = [
+  {
+    author: `Ivan`,
+    content: `Ну неужели кто-то может не любить такой великолепный сериал?! По-моему это один из самых потрясающих сериалов - отличная режиссерская работа, отличная игра актёров, отличный юмор, всё отлично!`,
+    date: `2019/12/31 23:59`,
+    emoji: `angry.png`
+  },
+  {
+    author: `John`,
+    content: `И чего все расхваливают этот сериал. Это же примитив. `,
+    date: `2 days ago`,
+    emoji: `puke.png`
+  },
+  {
+    author: `Mary`,
+    content: `В наше время популярности различного рода телесериалов, можно лицезреть сотни, может даже тысячи, представителей данного направления, всех жанров.`,
+    date: `10 days ago`,
+    emoji: `sleeping.png`
+  },
+  {
+    author: `DUD`,
+    content: `Не знаю, как вы, но я всегда ассоциировала себя и своих друзей с персонажами сериала, находя ну очень знакомые черты от каждого. `,
+    date: `Today`,
+    emoji: `smile.png`
+  },
+  {
+    author: `Man`,
+    content: `Перечитывая на данном сайте рецензии на сериал «Игрушки», наткнулся на такую фразу: «Может лучше посмотреть „Друзей“?»`,
+    date: ` 2010/10/1 13:25`,
+    emoji: `puke.png`
+  }
+];
+
+export const RANK_USER = [
+  `novice`,
+  `fan`,
+  `movie buff`
+];
